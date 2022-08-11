6d86c93b9ef763220b9c53bb45ad9249e6101592
osvaldo <iosvaldo.app@gmail.com>
2022-08-11 16:22:45 -0400

    initial commit 😅

---
+++ src/__tests__/Article.test.js
@@ -0,0 +1,16 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+import Article from "../components/Article";
+
+test("displays the text 'please pass this test'", () => {
+  render(<Article />);
+
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug(element);
+
+  // expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  expect(element).toBeInTheDocument();
+});
